14ff38b830c5ec392ee876f948473be46fa508eb
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2023-04-08 10:42:44 +0800

    Initial Commit

---
+++ jpeg_helper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  jpeg_helper
+//
+//  Created by jichen on 2023/4/8.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
