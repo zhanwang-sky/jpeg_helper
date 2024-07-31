5af947a507998a331e67de4cda9c71c52db3ec2e
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2024-07-31 15:47:18 +0800

    Initial Commit

---
+++ jpeg_helper/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  jpeg_helper
+//
+//  Created by zhanwang-sky on 2024/7/31.
+//
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
